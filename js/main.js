4aefa1703281ba1163865f2c37aa51512b4bcdfe
Dakhani Usman <dakhaniusman@gmail.com>
2024-11-18 20:10:03 +0530

    updates

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ let nav = document.querySelector(".navigation-wrap");
 
 window.onscroll = function () {
 
-    if (document.documentElement.scrollTop > 20){
-        nav.classList.add(".scroll-on")
+    if (document.documentElement.scrollTop > 0){
+        nav.classList.add("scroll-on")
     }
     else{
-        nav.classList.remove(".scroll-on")
+        nav.classList.remove("scroll-on")
     }
 }
 
